07128989b6fd803d8a7c1a0086d6db8c38ef5763
irenejoy15 <34486370+irenejoy15@users.noreply.github.com>
2024-10-08 11:33:00 +0800

    JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,19 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
