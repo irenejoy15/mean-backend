23228fd87535d396e1a551bdc284ec659aa38c99
Cefrian Yves Baldoza Trinchera <34486370+irenejoy15@users.noreply.github.com>
2024-11-19 16:05:34 +0800

    Revision Post Model and Migration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,7 @@ class Post extends Model
         'title',
         'content',
         'imagePath',
+        'creator'
     ];
 
     protected $casts = [
--- database/migrations/2024_10_08_081916_create_posts_table.php
+++ database/migrations/2024_10_08_081916_create_posts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('content');
             $table->string('imagePath');
+            $table->string('creator');
             $table->timestamps();
         });
     }
